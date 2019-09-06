d754a98ff104ffb40329b996ac43bb33b8e25ce8
xujialong <xjlong@outlook.com>
2019-09-06 10:25:06 +0800

    使用go mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/0990/goaction
+
+go 1.12
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/0990/goaction/hello"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	fmt.Println(hello.Greet())
 	fmt.Print("hello")
+	logrus.Info("fdsfsf")
 }
